e6469fd4fcea5f41e1b8797d5089ac59435a6626
Bryan Astacio <brya0196@gmail.com>
2019-02-20 23:09:26 -0400

    initial commit

---
+++ People.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace People.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ People.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace People.Data
+{
+    public class Class1
+    {
+    }
+}
