ac661eb1bc791d12dbb0ca1730ab2546748cd577
Bryan Astacio <brya0196@gmail.com>
2019-02-21 23:38:01 -0400

    added the people db context and users and roles entities

---
+++ People.Data/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace People.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int IdRole { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
--- People.Data/PeopleDbContext.cs
+++ People.Data/PeopleDbContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using People.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace People.Data
 {
-    public class PeopleDbContext
+    public class PeopleDbContext : DbContext
     {
+        public PeopleDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<KindService> kindServices { get; set; }
+        public DbSet<>
     }
 }
