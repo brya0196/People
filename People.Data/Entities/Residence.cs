3a4250d9b11cb45c1da8269317bde5f41a9754a2
Bryan Astacio <brya0196@gmail.com>
2019-02-24 14:49:31 -0400

    added the IdUser to residence entity

--- People.Data/Entities/Residence.cs
+++ People.Data/Entities/Residence.cs
@@ -8,8 +8,9 @@ namespace People.Data.Entities
     {
         public int Id { get; set; }
         public int IdCity { get; set; }
-
         public City City { get; set; }
+        public int IdUser { get; set; }
+
         public List<Person> People { get; set; }
         public List<Service> Services { get; set; }
     }
