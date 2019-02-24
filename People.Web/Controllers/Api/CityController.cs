cc89ccfe7ff76e2079a67a5e03eed89c9f219253
Bryan Astacio <brya0196@gmail.com>
2019-02-24 16:43:41 -0400

    added the city and proince and controller

---
+++ People.Web/Controllers/Api/CityController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using People.Core.Repositories;
+using People.Data.Entities;
+
+namespace People.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly CityRepository _cityRepository;
+
+        public CityController(CityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _cityRepository.GetAll();
+
+            if (!result.Any()) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetById/{Id:int}")]
+        public IActionResult GetById(int Id)
+        {
+            var result = _cityRepository.GetById(Id);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody]City city)
+        {
+            if (city == null) return BadRequest();
+
+            await _cityRepository.Add(city);
+
+            return Ok(city);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody]City city)
+        {
+            if (city == null) return BadRequest();
+
+            await _cityRepository.Update(city);
+
+            return Ok(city);
+        }
+
+        [HttpDelete("Delete/{Id:int}")]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            if (Id == 0) return BadRequest();
+
+            await _cityRepository.Delete(Id);
+
+            return Ok("Deleted");
+        }
+    }
+}
---
+++ People.Web/Controllers/Api/ProvinceController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using People.Core.Repositories;
+using People.Data.Entities;
+
+namespace People.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvinceController : ControllerBase
+    {
+        private readonly ProvinceRepository _provinceRepository;
+
+        public ProvinceController(ProvinceRepository provinceRepository)
+        {
+            _provinceRepository = provinceRepository;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _provinceRepository.GetAll();
+
+            if (!result.Any()) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetById/{Id:int}")]
+        public IActionResult GetById(int Id)
+        {
+            var result = _provinceRepository.GetById(Id);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody]Province province)
+        {
+            if (province == null) return BadRequest();
+
+            await _provinceRepository.Add(province);
+
+            return Ok(province);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody]Province province)
+        {
+            if (province == null) return BadRequest();
+
+            await _provinceRepository.Update(province);
+
+            return Ok(province);
+        }
+
+        [HttpDelete("Delete/{Id:int}")]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            if (Id == 0) return BadRequest();
+
+            await _provinceRepository.Delete(Id);
+
+            return Ok("Deleted");
+        }
+    }
+}
