9d406a7c3bfecf4759488ef7c470c7c1e9d63daf
Bryan Astacio <brya0196@gmail.com>
2019-02-25 00:00:29 -0400

    added the resident form

---
+++ People.Web/wwwroot/angular/common/directives/resident-form/resident-form.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('resident_form', resident_form);
+
+    resident_form.$inject = ['$window'];
+
+    function resident_form($window) {
+        // Usage:
+        //     <resident_form></resident_form>
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'E',
+            templeteUrl: '/angular/common/directives/resident-form.html'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+        }
+    }
+
+})();
