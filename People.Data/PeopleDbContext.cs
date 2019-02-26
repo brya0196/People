60d0b38d8e5d401c9a3368d2fc2e0c71b9691c9e
Bryan Astacio <brya0196@gmail.com>
2019-02-25 23:33:03 -0400

    added the people seed data

--- People.Data/PeopleDbContext.cs
+++ People.Data/PeopleDbContext.cs
@@ -28,12 +28,29 @@ namespace People.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Province>().HasKey(p => p.Id);
             modelBuilder.Entity<Province>().Property(p => p.Id).UseMySqlIdentityColumn();
+            modelBuilder.Entity<Province>().HasData(
+               new Province { Id = 1, Name = "Provincia 1" },
+               new Province { Id = 2, Name = "Provincia 2" },
+               new Province { Id = 3, Name = "Provincia 3" }
+           );
 
             modelBuilder.Entity<City>().HasKey(p => p.Id);
             modelBuilder.Entity<City>().Property(p => p.Id).UseMySqlIdentityColumn();
+            modelBuilder.Entity<City>().HasData(
+               new City { Id = 1, Name = "Ciudad 1", IdProvince = 1 },
+               new City { Id = 2, Name = "Ciudad 2", IdProvince = 2 },
+               new City { Id = 3, Name = "Ciudad 3", IdProvince = 3 }
+           );
 
             modelBuilder.Entity<KindService>().HasKey(p => p.Id);
             modelBuilder.Entity<KindService>().Property(p => p.Id).UseMySqlIdentityColumn();
+            modelBuilder.Entity<KindService>().HasData(
+                new KindService { Id = 1, Name = "Luz" },
+                new KindService { Id = 2, Name = "Agua" },
+                new KindService { Id = 3, Name = "Entretenimiento" },
+                new KindService { Id = 4, Name = "Salud" },
+                new KindService { Id = 5, Name = "Estudios" }
+            );
 
             modelBuilder.Entity<Residence>().HasKey(p => p.Id);
             modelBuilder.Entity<Residence>().Property(p => p.Id).UseMySqlIdentityColumn();
---
+++ People.Web/Migrations/20190226033155_AddedSeedData.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace People.Web.Migrations
+{
+    public partial class AddedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "IdProvince", "Name", "ProvinceId" },
+                values: new object[,]
+                {
+                    { 1, 1, "Ciudad 1", null },
+                    { 2, 2, "Ciudad 2", null },
+                    { 3, 3, "Ciudad 3", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Provinces",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Provincia 1" },
+                    { 2, "Provincia 2" },
+                    { 3, "Provincia 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "kindServices",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Luz" },
+                    { 2, "Agua" },
+                    { 3, "Entretenimiento" },
+                    { 4, "Salud" },
+                    { 5, "Estudios" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Provinces",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Provinces",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Provinces",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "kindServices",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "kindServices",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "kindServices",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "kindServices",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "kindServices",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
