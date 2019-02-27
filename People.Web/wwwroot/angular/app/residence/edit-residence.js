8a96c0c70cc90c2211439c0c8aa288428ba66be4
Bryan Astacio <brya0196@gmail.com>
2019-02-27 16:28:07 -0400

    eliminado codigo inecesario para el edit-residence

---
+++ People.Web/wwwroot/angular/app/residence/edit-residence.js
@@ -0,0 +1,83 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').component('editResidence', {
+        templateUrl: "/angular/app/residence/form-residence.html",
+        controller: AddResidenceController,
+        controllerAs: 'vm'
+    });
+
+    AddResidenceController.$inject = ['$q', '$log', 'kindServiceFactory', 'ProvinceFactory', 'CityProvince', 'ResidenceFactory', 'ServiceFactory', 'PersonService'];
+
+    function AddResidenceController($q, $log, kindServiceFactory, ProvinceFactory, CityProvince, ResidenceFactory, ServiceFactory, PersonService) {
+        var vm = this;
+
+        activate();
+        vm.residence = {};
+        vm.residents = [];
+        vm.resident = {};
+
+        vm.services = [];
+        vm.service = {};
+
+        //functions
+        vm.saveResident = saveResident;
+        vm.deleteResident = deleteResident;
+        vm.updateResident = updateResident;
+
+        vm.saveService = saveService;
+        vm.deleteService = deleteService;
+        vm.updateService = updateService;
+
+        vm.getCity = getCity;
+
+        vm.clean = clean;
+        vm.save = save;
+
+        function activate() {
+            ProvinceFactory.getAll().then(function (response) {
+                vm.provinces = response.data;
+                return vm.provinces;
+            });
+        }
+
+        function saveResident() {
+           
+        }
+
+        function deleteResident(id) {
+           
+        }
+
+        function updateResident(id) {
+           
+        }
+
+        function saveService() {
+            
+        }
+
+        function deleteService(id) {
+            
+        }
+
+        function updateService(id) {
+            
+        }
+
+        function clean() {
+            vm.resident = {};
+        }
+
+        function getCity(id) {
+            CityProvince.getByIdProvince(id).then(function (response) {
+                vm.cities = response.data;
+                return vm.cities;
+            });
+        }
+
+        function save() {
+            
+        }
+    }
+})();
