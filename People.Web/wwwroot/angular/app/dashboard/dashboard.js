684018103acb0ef4b195e2cd7f3bef4807694132
Bryan Astacio <brya0196@gmail.com>
2019-03-03 19:12:54 -0400

    added the services data

--- People.Web/wwwroot/angular/app/dashboard/dashboard.js
+++ People.Web/wwwroot/angular/app/dashboard/dashboard.js
@@ -1,21 +1,52 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('core').component('dashboard', {
-        templateUrl: '/angular/app/dashboard.html',
+    angular.module('app').component('dashboard', {
+        templateUrl: '/angular/app/dashboard/dashboard.html',
         controller: DashboardController,
         controllerAs: 'vm'
     });
 
-    DashboardController.$inject = [];
+    DashboardController.$inject = ['$q', '$log', 'ServiceFactory', 'kindServiceFactory', 'PersonaFactory'];
 
-    function DashboardController() {
+    function DashboardController($q, $log, ServiceFactory, kindServiceFactory, PersonaFactory) {
         var vm = this;
 
         vm.$onInit = onInit;
 
         function onInit() {
+            var data = [
+                ServiceFactory.getAll(),
+                kindServiceFactory.getAll()
+            ];
 
+
+            $q.all(data).then(function (response) {
+                var service = response[0].data;
+                var kindService = response[1].data; 
+
+                vm.serviceData = {
+                    data: [0, 0, 0, 0, 0],
+                    labels: []
+                };
+
+                angular.forEach(kindService, function (item) {
+                    vm.serviceData.labels.push(item.name);
+                    return vm.serviceData;
+                });
+
+                angular.forEach(service, function (item) {
+                    if (item.idKindService === 1) vm.serviceData.data[0] += 1;
+                    if (item.idKindService === 2) vm.serviceData.data[1] += 1;
+                    if (item.idKindService === 3) vm.serviceData.data[2] += 1;
+                    if (item.idKindService === 4) vm.serviceData.data[3] += 1;
+                    if (item.idKindService === 5) vm.serviceData.data[4] += 1;
+                    return vm.serviceData;
+                });
+                
+
+                return response;
+            });
         }
     }
 })();
---
+++ People.Web/wwwroot/angular/common/directives/pie-chart/pie-chart.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').directive('pieChart', pieChart);
+
+    pieChart.$inject = ['$window', '$log'];
+
+    function pieChart($window, $log) {
+        // Usage:
+        //     <pie-chart></pie-chart>
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'E',
+            scope: {
+                data: '=',
+                lebels: '='
+            },
+            template: '<div class="card"><canvas class="card-body" id="pieChart_{{id}}"></canvas></div>'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            scope.id = attrs.id;
+            angular.element(element).ready(function () {
+                var ctx = angular.element("#pieChart_" + scope.id);
+
+                var pieChart = new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        datasets: [
+                            {
+                                data: [],
+                                backgroundColor: ['#ff6384', '#fd7e14', '#dc3545', '#20c997', '#007bff']
+                            }
+                        ],
+                        labels: [],
+                    },
+                    options: {}
+                });
+
+                scope.$watch('data', function () {
+                    pieChart.data.datasets[0].data = scope.data;
+                    pieChart.update();
+                });
+
+                scope.$watch('lebels', function () {
+                    pieChart.data.labels = scope.lebels;
+                    pieChart.update();
+                });
+            });
+        }
+    }
+})();
